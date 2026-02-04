40e1e95612284040585b3be2cb153a35183afa8f
samiksha132 <samikshapuramshettiwar@gmail.com>
2026-02-04 10:33:21 +0530

    Create User class with constructor and add method

---
+++ user.java
@@ -0,0 +1,16 @@
+public class User{
+   int a ;
+   int b ;
+  public User(int a, int b){
+     this.a = a; 
+     this.b = b;
+  }
+  
+   public void add(int a, int b){
+       int c = a + b;
+   }
+
+public static void main(String[] args){
+    User user = new User(10, 20);
+  }
+}
